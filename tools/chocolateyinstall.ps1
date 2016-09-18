561e1d600d06dc8da20be80b95150552ff66f4ae
Yatao Li <yatao.li@live.com>
2016-09-18 15:08:54 +0800

    initial commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,41 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'netease-cloudmusic'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://s1.music.126.net/download/pc/cloudmusicsetup_2_1_0[145894].exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+
+  silentArgs   = '/S'
+  validExitCodes= @(0)
+
+  softwareName  = '网易云音乐*'
+  checksum      = '2e4b619fe37a77b5ca57bf74660bd6f9'
+  checksumType  = 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
